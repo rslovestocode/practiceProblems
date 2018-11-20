21aad640be4a18041fabd97c18b54e212ee9b11b
rslovestocode <39102946+rslovestocode@users.noreply.github.com>
2018-11-19 20:00:18 -0800

    Create HouseRobber_198.java

---
+++ DP/HouseRobber_198.java
@@ -0,0 +1,44 @@
+/*
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+
+Example 1:
+
+Input: [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+             Total amount you can rob = 1 + 3 = 4.
+Example 2:
+
+Input: [2,7,9,3,1]
+Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+             Total amount you can rob = 2 + 9 + 1 = 12.
+
+*/
+
+
+class Solution {
+    public int rob(int[] num) {
+    int prevNo = 0;
+    int prevYes = 0;
+    for (int n : num) {
+        int temp = prevNo;
+        prevNo = Math.max(prevNo, prevYes);// not including this house -- may/maynot include prev
+        prevYes = n + temp;  //including this .. with prevNo 
+    }
+    return Math.max(prevNo, prevYes);
+}
+}
+/*
+public int rob(int[] num) {
+    int[][] dp = new int[num.length + 1][2];
+    for (int i = 1; i <= num.length; i++) {
+        dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]);
+        dp[i][1] = num[i - 1] + dp[i - 1][0];
+    }
+    return Math.max(dp[num.length][0], dp[num.length][1]);
+}
+
+*/
